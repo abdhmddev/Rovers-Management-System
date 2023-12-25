8fcf00d685f8e96eea4db50d7b9cc8cbb848ad71
Abdulrahman Hammoud <abdhammoud.dev@gmail.com>
2023-12-25 16:45:35 +0200

    signup radio

--- signup.php
+++ signup.php
@@ -35,6 +35,16 @@
                     <label for="exampleInputPassword1" class="form-label">تأكيد كلمة السر</label>
                     <input type="password" name="password_confirm" class="form-control" id="exampleInputPassword1">
                 </div>
+                <div class="radio">
+                    <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                    <label class="form-check-label " for="inlineRadio1" >جوال </label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                    <label class="form-check-label" for="inlineRadio2">قائد</label>
+                </div>
+                </div>
                 <div class="mb-3 ">
                     <div><a href="login.php">لديك حساب؟ سجل الدخول</a></div>
                 </div>
